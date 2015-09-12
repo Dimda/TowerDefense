e178507cb9020df5e1adf7c9f1574b241fd09b72
Davis Kemlers <daviskemlers@gmail.com>
2015-09-12 21:54:07 +0900

    ignoreファイル追加

--- Assets/Scripts/ShowUI.cs
+++ Assets/Scripts/ShowUI.cs
@@ -13,7 +13,6 @@ public class ShowUI : MonoBehaviour {
 	}
 	void OnMouseDown()
 	{
-
 		Destroy (gameObject);
 
 		if(GameObject.Find("Build").GetComponent<UnityEngine.UI.Image>().enabled == true)
@@ -21,6 +20,7 @@ public class ShowUI : MonoBehaviour {
 			GameObject.Find("Build").GetComponent<UnityEngine.UI.Image>().enabled = false;
 			GameObject.Find("Text").GetComponent<UnityEngine.UI.Text>().enabled = false;
 		}else{
+			GameObject.Find("Build").transform.localPosition = new Vector3(Input.mousePosition.x -870, Input.mousePosition.y - 250, 100);
 			GameObject.Find("Build").GetComponent<UnityEngine.UI.Image>().enabled = true;
 			GameObject.Find("Text").GetComponent<UnityEngine.UI.Text>().enabled = true;
 		}
