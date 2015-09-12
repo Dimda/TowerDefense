5dc90e64ac3ea9ee2c5f886cf317e8ee6a32dcf6
Davis Kemlers <daviskemlers@gmail.com>
2015-09-12 20:46:38 +0900

    UI起動できた

---
+++ Assets/Scripts/ShowUI.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ShowUI : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	private void Update () 
+	{
+	}
+	void OnMouseDown()
+	{
+
+		Destroy (gameObject);
+
+		if(GameObject.Find("Build").GetComponent<UnityEngine.UI.Image>().enabled == true)
+		{
+			GameObject.Find("Build").GetComponent<UnityEngine.UI.Image>().enabled = false;
+			GameObject.Find("Text").GetComponent<UnityEngine.UI.Text>().enabled = false;
+		}else{
+			GameObject.Find("Build").GetComponent<UnityEngine.UI.Image>().enabled = true;
+			GameObject.Find("Text").GetComponent<UnityEngine.UI.Text>().enabled = true;
+		}
+
+	}
+}
+
