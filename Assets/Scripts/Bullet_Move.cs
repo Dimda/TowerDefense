a1f01b58e511eb9079da65c85581e1d18da52596
irb <kutuzov1228@gmail.com>
2015-09-12 22:02:07 +0900

    tower01

---
+++ Assets/Scripts/Bullet_Move.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet_Move : MonoBehaviour
+{
+    public float speed = 1f;
+    // Use this for initialization
+    void Start()
+    {
+        this.transform.parent = GameObject.Find("Cube").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localPosition = new Vector3(speed, 0, 0);
+        speed += 1f;
+    }
+}
---
+++ Assets/Scripts/Bullet_Shot.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet_Shot : MonoBehaviour
+{
+    public GameObject prefab, gun;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(prefab,new Vector3(this.transform.position.x,this.transform.position.y,this.transform.position.z),this.transform.rotation);
+        }
+    }
+}
